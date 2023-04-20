ecd5528f148581bb1e07ddb5dc64682cb1450c08
jlizardi1 <122846079+jlizardi1@users.noreply.github.com>
2023-04-19 20:12:22 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
-function myMap() {
-  var mapProp = {
-    center: new google.maps.LatLng(41.87552, -87.630717),
-    zoom: 10,
-  };
-  var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-  myPointer(map);
+let map;
 
-  google.maps.event.addListener(map, "click", function(event){
-    movePtr(map, event.latLng);
+async function initMap() {
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
   });
 }
+
+initMap();

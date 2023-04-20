f0d3b7c62dd5054c7fba54f2cf30f2bfeb3817e2
jlizardi1 <122846079+jlizardi1@users.noreply.github.com>
2023-04-19 19:44:36 -0500

    Map corrections

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 var map;
 function initMap() {
-    
-    var myLatLng = {lat: 41.8781, lng: -87.6298};
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: myLatLng,
-        zoom: 12
+    const { Map } = await google.maps.importLibrary("maps");
+
+    map = new Map(document.getElementById("map"), {
+    center: { lat: 41.8781, lng: -87.6298},
+    zoom: 8
+
     });
 }

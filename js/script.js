085ded377467a4fe3c9c16387b95060770d5bd17
jlizardi1 <122846079+jlizardi1@users.noreply.github.com>
2023-04-24 20:58:47 -0500

    edits

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
 
   map = new Map(document.getElementById("map"), {
-    center: { lat: 41.8781, lng: 87.6298 },
+    center: { lat: 41.8781, lng: -87.6298 },
     zoom: 8,
   });
 }

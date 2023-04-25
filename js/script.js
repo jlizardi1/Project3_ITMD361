cae439b25001400f49f4658f8b121a1bf384dff4
jlizardi1 <122846079+jlizardi1@users.noreply.github.com>
2023-04-24 20:47:11 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,11 +5,10 @@ async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
 
   map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 41.8781, lng: 87.6298 },
     zoom: 8,
   });
 }
-
 initMap();
 
 var slideIndex = 0;
